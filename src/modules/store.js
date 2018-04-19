04c4187a3e2b648361bba054c9c6b3a960480a81
VasyaMaksymiv <mr.maksymiv97@gmail.com>
2018-04-19 17:03:39 +0300

    homework-7

---
+++ src/modules/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { autoRehydrate, persistStore } from 'redux-persist';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import reducer from './';
+
+const store = createStore(reducer, undefined, composeWithDevTools(applyMiddleware(thunkMiddleware), autoRehydrate()));
+
+persistStore(store, { whitelist: ['user'] });
+
+export default store;
